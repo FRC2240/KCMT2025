42124dce0cc4c11d1a9bde4a16a4b5cd762e0929
Laurens Schunk <laurensdschunk@gmail.com>
2025-09-10 15:25:50 -0600

    Swerve and renamed to funnel

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,9 @@
+package frc.robot;
+
+public final class Constants {
+  public static final double MAX_SPEED = 4;
+
+  public static class OperatorConstants {
+    public static final double DEADBAND = 0.5;
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/Funnel.java
@@ -0,0 +1,13 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+
+public class Funnel {
+    // Spin the funnel at a certain amerage
+    Command spinFunnelCommand() {
+
+        return Commands.runOnce(() -> {
+        });
+    }
+}
