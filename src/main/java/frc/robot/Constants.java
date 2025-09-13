074bcb574d64ea5021991398b0e3d20d986026ba
Laurens Schunk <laurensdschunk@gmail.com>
2025-09-13 15:13:48 -0600

    Sabertooth swerve configs

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -3,6 +3,10 @@ package frc.robot;
 public final class Constants {
   public static final double MAX_SPEED = 4;
 
+  public static class Funnel {
+    public static final int MOTOR_ID = 21;
+  }
+
   public static class OperatorConstants {
     public static final double DEADBAND = 0.5;
   }
