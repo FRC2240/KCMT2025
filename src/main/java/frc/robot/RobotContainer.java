6813c34a9a3a95f50c9016f609a4058b956d697d
Owen Rogers <locust5280o@gmail.com>
2025-10-11 20:21:57 -0600

    h

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -47,7 +47,6 @@ public class RobotContainer {
   private ManipulatorState currentState = Constants.ManipulatorStates.IDLE;
 
   public RobotContainer() {
-    addNamedCommands();
 
     switch (Constants.CURRENT_MODE) {
         case REAL:
@@ -68,126 +67,7 @@ public class RobotContainer {
             vision = new Vision(drivebase::addVisionMeasurement, new BaseVisionIO() {}, new BaseVisionIO() {});
             break;
     }
-    configureBindings();
     configureAutos();
-  }
-
-  private void addNamedCommands() {
-    // Stuff from c++ (Last years code)
-    NamedCommands.registerCommand("l4", Commands.print("start l4").andThen(setStateCommand(ManipulatorStates.L4)).andThen(Commands.print("end l4")));
-    NamedCommands.registerCommand("l2", Commands.print("start l2").andThen(setStateCommand(ManipulatorStates.L2)).andThen(Commands.print("end l2")));
-    NamedCommands.registerCommand("score_l4", Commands.print("start l4").andThen(setStateCommand(ManipulatorStates.L4)).andThen(scoreCommand()).andThen(Commands.print("end score_l4")));
-    NamedCommands.registerCommand("intake", Commands.print("start intake").andThen(setStateCommand(ManipulatorStates.INTAKE).alongWith(grabber.intakeCoralCommand())));
-    NamedCommands.registerCommand("idle", Commands.print("start idle").andThen(setStateCommand(ManipulatorStates.IDLE).alongWith(grabber.idleCommand())).andThen(Commands.print("end idle")));
-    NamedCommands.registerCommand("algae_intake", Commands.print("start algae intake").andThen(grabber.intakeAlgaeCommand()));
-    NamedCommands.registerCommand("algae_l2", Commands.print("start algae l2").andThen(setStateCommand(ManipulatorStates.ALGAE_L2)).andThen(Commands.print("end algae l2")));
-    NamedCommands.registerCommand("algae_score", Commands.print("start algae extake").andThen(grabber.extakeAlgaeCommand()));
-    NamedCommands.registerCommand("barge", Commands.print("start barge").andThen(setStateCommand(ManipulatorStates.BARGE)).andThen(Commands.print("end barge")));
-    // Add auto align here
-  }
-
-  private void configureBindings() {
-    // Default comand is to drive Field Oriented with Angular Velocity
-    drivebase.setDefaultCommand(drivebase.driveFieldOriented(drivebase.driveAngularVelocity));
-
-    funnel.setDefaultCommand(funnel.spinCommand());
-
-    // driverXbox.a().onTrue(drivebase.sysIdDriveMotorCommand());
-
-
-
-    // Intake coral
-    stick0.leftBumper().and(stick0.leftTrigger().negate())
-        .onTrue(setStateCommand(ManipulatorStates.INTAKE).alongWith(grabber.intakeCoralCommand()));
-
-    // Intake Algae from ground
-    stick0.leftBumper().and(stick0.leftTrigger())
-        .onTrue(setStateCommand(ManipulatorStates.GROUND_ALGAE).alongWith(grabber.intakeAlgaeCommand()));
-
-    // Goes to algae l2
-    stick0.a().and(stick0.leftTrigger())
-        .onTrue(setStateCommand(ManipulatorStates.ALGAE_L2).alongWith(grabber.intakeAlgaeCommand()));
-
-    // Goes to algae l3
-    stick0.x().and(stick0.leftTrigger())
-        .onTrue(setStateCommand(ManipulatorStates.ALGAE_L3).alongWith(grabber.intakeAlgaeCommand()));
-
-    // Goes to barge
-    stick0.leftTrigger().and(stick0.y())
-        .onTrue(setStateCommand(ManipulatorStates.BARGE));
-
-    // Goes to processor
-    stick0.b().and(stick0.leftTrigger())
-        .onTrue(setStateCommand(ManipulatorStates.PROCESSOR));
-
-    // Scores
-    stick0.rightBumper().and(stick0.leftTrigger().negate())
-        .onTrue(scoreCommand());
-
-    // Extakes algae
-    stick0.rightBumper().and(stick0.leftBumper())
-        .onTrue(grabber.extakeAlgaeCommand());
-
-    // Goes to l4
-    stick0.y().and(stick0.leftTrigger().negate())
-        .onTrue(setStateCommand(ManipulatorStates.L4));
-
-    // Goes to l3
-    stick0.x().and(stick0.leftTrigger().negate())
-        .onTrue(setStateCommand(ManipulatorStates.L3));
-
-    // Goes to l2
-    stick0.a().and(stick0.leftTrigger().negate())
-        .onTrue(setStateCommand(ManipulatorStates.L2));
-
-    // Goes to l1
-    stick0.b()
-        .onTrue(setStateCommand(ManipulatorStates.L1));
-
-    // Idle
-    stick0.rightTrigger()
-        .onTrue(setStateCommand(ManipulatorStates.IDLE).alongWith(grabber.idleCommand()));
-
-    // Rezero wrist
-    stick1.start()
-        .whileTrue(wrist.rezeroCommand());
-
-    // Offest wrist up
-    stick1.y()
-        .onTrue(wrist.offsetCommand(Constants.Wrist.OFFSET_AMOUNT));
-
-    // Offset wrist down
-    stick1.b()
-        .onTrue(wrist.offsetCommand(Constants.Wrist.OFFSET_AMOUNT.times(-1)));
-
-    // Offset elevator up
-    stick1.x()
-        .onTrue(elevator.offsetCommand(Constants.Elevator.OFFSET_AMOUNT));
-
-    // Offset elevator down
-    stick1.a()
-        .onTrue(elevator.offsetCommand(Constants.Elevator.OFFSET_AMOUNT.times(-1)));
-
-    // Align to left side
-    stick0.povLeft()
-        .onTrue(drivebase.alignCommand(Alignment.LEFT));
-
-
-    // Align to right side
-    stick0.povRight()
-        .onTrue(drivebase.alignCommand(Alignment.RIGHT));
-
-    // Ground algae intake
-    stick1.rightTrigger()
-        .onTrue(setStateCommand(ManipulatorStates.GROUND_ALGAE));
-
-    // Funnel toggle
-    stick1.leftTrigger()
-        .toggleOnTrue(funnel.stopCommand());
-
-    //stick2.a().onTrue(drivebase.sysIdDriveMotorCommand());
-    //stick2.b().onTrue(drivebase.sysIdAngleMotorCommand());
-    //stick2.x().onTrue(drivebase.fakeVisionMeasurement());
   }
 
   private void configureAutos(){
